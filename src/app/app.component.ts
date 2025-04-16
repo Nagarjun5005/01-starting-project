64578ed16495e5d8ff2b3d2c069f43a87503cbef
Nagarjun5005 <nagarjun20n@gmail.com>
2025-04-16 13:41:21 +0530

    used legacy ng for and if to render the components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,22 +3,26 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from '../dummy-users';
 import { TasksComponent } from './tasks/tasks.component';
+import { NgFor ,NgIf } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent,UserComponent,TasksComponent],
+  imports: [HeaderComponent,UserComponent,TasksComponent,NgFor,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
     users=DUMMY_USERS;
     //this is for app-task component
-    selectedUserId='u1';
+    // selectedUserId='u1';
+    selectedUserId?: string;
+    // selectedUserId?:string;
 
     //this is for app-task component
     get selectedUser(){
       return this.users.find((user)=>user.id===this.selectedUserId)!;
-   
+      console.log(this.selectedUserId);
+      console.log(this.selectedUser);
     }
 
     onSelectUser(id:string){
